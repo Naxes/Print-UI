f2ad918e0c96d89263df0f8dbe571169034a8282
Naxes <seanbickmore@icloud.com>
2022-11-26 20:28:32 +0000

    fixing rollup breaking change

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,21 @@
 import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import external from 'rollup-plugin-peer-deps-external';
 import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
 
-const packageJson = require('./package.json');
-
 export default [
   {
     input: './src/index.ts',
     output: [
       {
-        file: packageJson.main,
+        file: 'dist/cjs/index.js',
         format: 'cjs',
       },
       {
-        file: packageJson.module,
+        file: 'dist/esm/index.js',
         format: 'esm',
       },
     ],
